8ecdd3d80b1903dc45ae159209bd2fbd8c589509
quailfish32 <sprinkles>
2022-07-02 14:57:47 -0600

    phase 2

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 import tcod
-from actions import EscapeAction, MovementAction
+from engine import Engine
+from game_map import GameMap
 from input_handlers import EventHandler
+from entity import Entity
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    map_width = 80
+    map_height = 45
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -16,33 +18,28 @@ def main() -> None:
 
     event_handler = EventHandler()
 
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 0, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 255))
+    entities = {npc, player}
+
+    game_map = GameMap(map_width, map_height)
+
+    My_engine = Engine(entities, event_handler, game_map, player)
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Sprinkles Dungeon Adventures",
+        title="Sprinkles Future Triggering Adventure",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
-            context.present(root_console)
+            My_engine.render(root_console, context)
+            
+            events = tcod.event.wait()
 
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                action =  event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, EscapeAction):
-                    raise SystemExit()
-
-                if isinstance(action, MovementAction):
-                    player_x = action.dx + player_x
-                    player_y += action.dy
-
+            My_engine.handle_events(events)
 
 if __name__ == "__main__":
     main()
